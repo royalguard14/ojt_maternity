d9ee04aa50594d464e1be28c4537a84fb27b4a2a
royalguard14 <ghaizar.bautista@aclcbutuan.edu.ph>
2025-05-23 00:00:31 +0800

    awit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,62 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PHILCRIS-FORM-102</title>
 
-                <div class="card-body">
-                  <form method="POST" action="{{ route('login') }}">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css?v=3.2.0">
+
+
+
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+
+<img class="img-circle" src="../../dist/img/logo.jpg" style="width:50%">
+
+      <a href="../../index2.html" class="h1" style="font-size:2rem"><b>Information</b>System</a>
+    </div>
+    <div class="card-body">
+
+                 <form method="POST" action="{{ route('login') }}">
     @csrf
 
-    <div class="row mb-3">
-        <label for="login" class="col-md-4 col-form-label text-md-end">{{ __('Username or Email Address') }}</label>
+      <p class="login-box-msg">Sign in to start your session</p>
 
-        <div class="col-md-6">
-            <!-- Blank input field without old() value -->
-            <input type="text" id="login" name="login" class="form-control @error('login') is-invalid @enderror" required autofocus>
+      
+        <div class="input-group mb-3">
+          <input id="login" name="login" type="text" class="form-control @error('login') is-invalid @enderror" placeholder="Username or Email Address" required autofocus>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
 
-            @error('login')
+                  @error('login')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
         </div>
-    </div>
-
-    <div class="row mb-3">
-        <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-        <div class="col-md-6">
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+        <div class="input-group mb-3">
+          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
 
             @error('password')
                 <span class="invalid-feedback" role="alert">
@@ -38,38 +64,37 @@
                 </span>
             @enderror
         </div>
-    </div>
-
-    <div class="row mb-3">
-        <div class="col-md-6 offset-md-4">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                <label class="form-check-label" for="remember">
-                    {{ __('Remember Me') }}
-                </label>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
             </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
         </div>
-    </div>
+      </form>
 
-    <div class="row mb-0">
-        <div class="col-md-8 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-                {{ __('Login') }}
-            </button>
 
-            @if (Route::has('password.request'))
-                <a class="btn btn-link" href="{{ route('password.request') }}">
-                    {{ __('Forgot Your Password?') }}
-                </a>
-            @endif
-        </div>
     </div>
-</form>
-
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
 </div>
-@endsection
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js?v=3.2.0"></script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"943d5e2fdd848619","serverTiming":{"name":{"cfExtPri":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}},"version":"2025.4.0-1-g37f21b1","token":"2437d112162f4ec4b63c3ca0eb38fb20"}' crossorigin="anonymous"></script>
+</body>
+</html>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -31,17 +31,22 @@
 <!-- jQuery -->
 <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('dist/js/chart.js') }}"></script>
-
+<style type="text/css">
+  
+  .labo8, .card-body {
+    opacity: 0.8;
+  }
+</style>
 @yield('style')
 
 
 </head>
 <body class="hold-transition sidebar-mini layout-footer-fixed">
-  <div class="wrapper">
+  <div class="wrapper ">
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
       <!-- Left navbar links -->
-      <ul class="navbar-nav">
+      <ul class="navbar-nav ">
         <li class="nav-item">
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
@@ -80,7 +85,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('dist/img/logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">{{ config('app.custom_name') }}</span>
     </a>
     <!-- Sidebar -->
@@ -183,7 +188,13 @@
  <!-- /.sidebar -->
 </aside>
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
+<div class="content-wrapper" 
+
+
+style="background: url('{{ asset('dist/img/bgc.jpg') }}') no-repeat center center fixed; background-size: cover;"
+
+
+>
   <!-- Content Header (Page header) -->
   <div class="content-header">
     <div class="container-fluid">
@@ -216,13 +227,13 @@
 <!-- /.control-sidebar -->
 @enddeveloper
 <!-- Main Footer -->
-<footer class="main-footer">
+<footer class="main-footer footline labo8" >
   <!-- To the right -->
   <div class="float-right d-none d-sm-inline">
     {{ config('app.custom_name') }}
   </div>
   <!-- Default to the left -->
-  <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io"></a>.</strong> All rights reserved.
+     <strong>Copyright &copy; 2025 <a href="https://www.facebook.com/profile.php?id=61572728822378">MGX TECH</a>.</strong> All rights reserved.
 </footer>
 </div>
 <!-- ./wrapper -->
